3113df02e52ffd0a6b645c4e1ffdcd767889e571
Filipp Voronov <fivoronov@gmail.com>
2022-04-08 22:15:11 +0300

    Failed CI

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -4,6 +4,13 @@ public class BonusServiceTest {
 
     @org.junit.jupiter.api.Test
     void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
 
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
     }
 }
